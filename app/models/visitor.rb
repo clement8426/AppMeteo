273e871d1a7f776bd0fce76bd4c2c2205db5ff99
clement bastien bastienclement826@gmail.com <bastienclement826@gmail.com>
2024-02-07 03:27:29 +0100

    add validation 30 user to db

--- app/models/visitor.rb
+++ app/models/visitor.rb
@@ -9,4 +9,18 @@ class Visitor < ApplicationRecord
   validates :conditionText, presence: true
   validates :precipitation, presence: true
   validates :dayOrNight, presence: true
+
+  after_create :cleanup_old_visitors
+
+  private
+
+  def cleanup_old_visitors
+    excess_visitors_count = Visitor.count - 30
+
+    if excess_visitors_count > 0
+      visitors_to_delete = Visitor.order(created_at: :asc).limit(excess_visitors_count)
+      visitors_to_delete.destroy_all
+    end
+  end
+
 end
