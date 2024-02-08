1e26d817205ccec78fb7cac4f31d5f6cec18e751
Johan FILONNEAU <filonovjohan@gmail.com>
2024-02-08 19:37:37 +0100

    fixer probleme de DOM avec la fonction JS qui arretait de boucler quand il manquait des elements dans le HTML, arretant d'afficher les datas apès la derniere boucle qui renvoi nill

--- app/javascript/ip_api.js
+++ app/javascript/ip_api.js
@@ -24,8 +24,6 @@ function callAPIWithUserIP(userIP) {
       width="80"
       alt="${data.country_name}"
       >`;
-
-
       // Call the weather API with the retrieved city
       callWeatherAPI(data.city, userIP, data);
     })
@@ -42,46 +40,65 @@ function callWeatherAPI(city, userIP, locationData) {
     .then((weatherData) => {
       const weatherInfo = document.getElementById("weather-info");
       const weatherInfoTown = document.getElementById("weather-info-town");
-      const weatherInfoCountry = document.getElementById(
-        "weather-info-country"
-      );
-      const weatherInfoTemperature = document.getElementById(
-        "weather-info-temperature"
-      );
+      const weatherInfoCountry = document.getElementById("weather-info-country");
+      const weatherInfoTemperature = document.getElementById("weather-info-temperature");
       const weatherInfoWind = document.getElementById("weather-info-wind");
-      const weatherInfoCondition = document.getElementById(
-        "weather-info-condition"
-      );
-      const weatherInfoPrecipitation = document.getElementById(
-        "weather-info-precipitation"
-      );
+      const weatherInfoCondition = document.getElementById("weather-info-condition");
+      const weatherInfoPrecipitation = document.getElementById("weather-info-precipitation");
       const weatherInfoDayOrNight = document.getElementById("weather-info-day-or-night");
-      const isDay = weatherData.current.is_day;
-
-      if (isDay) {
-          weatherInfoDayOrNight.innerHTML = '<img src="../../assets/soleil.png" alt="Soleil" width="30px" height="30px">';
-      } else {
-          weatherInfoDayOrNight.innerHTML = '<img src="../../assets/lune.png" alt="Lune" width="30px" height="30px">';
-      }
       const weatherInfoCloud = document.getElementById("weather-info-cloud");
 
-      // Déterminez si le ciel est dégagé ou nuageux
-      const isClearSky = weatherData.current.cloud === 0;
-      if (isClearSky) {
-        weatherInfoCloud.innerHTML = '<img src="../../assets/cloudy_sky.png" alt="Nuageux" width="50px" height="50px">';
-
+      if (weatherInfoWind) {
+        weatherInfoWind.innerHTML = `${weatherData.current.wind_kph} km/h`;
       } else {
-        weatherInfoCloud.innerHTML = '<img src="../../assets/clear_sky.png" alt="Ciel clair" width="50px" height="50px">';
-
+        console.error("Element weather-info-wind not found in the DOM.");
       }
-console.log(weatherData);
-      weatherInfoCountry.innerHTML = `${weatherData.location.country}`;
-      weatherInfoTown.innerHTML = `${weatherData.location.name}`;
-      weatherInfoTemperature.innerHTML = `${weatherData.current.temp_c}°C`;
-      weatherInfoWind.innerHTML = `${weatherData.current.wind_kph} km/h`;
 
-      weatherInfoCondition.innerHTML = `${weatherData.current.condition.text}`;
-      weatherInfoPrecipitation.innerHTML = `${weatherData.current.precip_mm} mm`;
+      if (weatherInfoDayOrNight) {
+        const isDay = weatherData.current.is_day;
+        weatherInfoDayOrNight.innerHTML = isDay ? '<img src="../../assets/soleil.png" alt="Soleil" width="30px" height="30px">' : '<img src="../../assets/lune.png" alt="Lune" width="30px" height="30px">';
+      } else {
+        console.error("Element weather-info-day-or-night not found in the DOM.");
+      }
+
+      if (weatherInfoCloud) {
+        const isClearSky = weatherData.current.cloud === 0;
+        weatherInfoCloud.innerHTML = isClearSky ? '<img src="../../assets/cloudy_sky.png" alt="Nuageux" width="50px" height="50px">' : '<img src="../../assets/clear_sky.png" alt="Ciel clair" width="50px" height="50px">';
+      } else {
+        console.error("Element weather-info-cloud not found in the DOM.");
+      }
+
+      console.log(weatherData);
+
+      if (weatherInfoCountry) {
+        weatherInfoCountry.innerHTML = `${weatherData.location.country}`;
+      } else {
+        console.error("Element weather-info-country not found in the DOM.");
+      }
+
+      if (weatherInfoTown) {
+        weatherInfoTown.innerHTML = `${weatherData.location.name}`;
+      } else {
+        console.error("Element weather-info-town not found in the DOM.");
+      }
+
+      if (weatherInfoTemperature) {
+        weatherInfoTemperature.innerHTML = `${weatherData.current.temp_c}°C`;
+      } else {
+        console.error("Element weather-info-temperature not found in the DOM.");
+      }
+
+      if (weatherInfoCondition) {
+        weatherInfoCondition.innerHTML = `${weatherData.current.condition.text}`;
+      } else {
+        console.error("Element weather-info-condition not found in the DOM.");
+      }
+
+      if (weatherInfoPrecipitation) {
+        weatherInfoPrecipitation.innerHTML = `${weatherData.current.precip_mm} mm`;
+      } else {
+        console.error("Element weather-info-precipitation not found in the DOM.");
+      }
 
       // Combiner les données de localisation et de météo dans un seul objet
       const postData = {
@@ -113,18 +130,13 @@ console.log(weatherData);
           }
         })
         .catch((error) => {
-          console.error(
-            "Erreur lors de l'envoi des données au serveur:",
-            error
-          );
+          console.error("Erreur lors de l'envoi des données au serveur:", error);
         });
     })
     .catch((error) => {
-      console.error(
-        "Une erreur s'est produite lors de l'appel de l'API météo : ",
-        error
-      );
+      console.error("Une erreur s'est produite lors de l'appel de l'API météo : ", error);
     });
 }
+
 
 getUserIP(callAPIWithUserIP);
